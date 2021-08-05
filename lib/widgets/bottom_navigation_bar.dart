7458024cc2be845b533729398c390857a172ec6e
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2021-08-05 13:28:34 +0500

    bottom navigation bar

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -8,6 +8,33 @@ class BottomNavigationBarTravel extends StatefulWidget {
 }
 
 class _BottomNavigationBarTravelState extends State<BottomNavigationBarTravel> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    Icon(
+      Icons.call,
+      size: 150,
+    ),
+    Icon(
+      Icons.camera,
+      size: 150,
+    ),
+    Icon(
+      Icons.chat,
+      size: 150,
+    ),
+    // Icon(
+    //   Icons.chat,
+    //   size: 150,
+    // ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,9 +49,32 @@ class _BottomNavigationBarTravelState extends State<BottomNavigationBarTravel> {
             offset: Offset(0, 5),
           ),
         ],
-        // borderRadius: BorderRadius.only(
-        //   topLeft:
-        // ),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call),
+            label: 'Calls',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera),
+            label: 'Camera',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.chat),
+          //   label: 'Chats',
+          // ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
